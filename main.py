b96d7267de2604a3609bde5e7ad9d35400832aa1
Tobias <tobias.aditya@gmail.com>
2023-12-25 19:32:49 +0700

    add path param and enum

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class Gender(str,Enum):
+    LAKI_LAKI = "LAKI-LAKI"
+    PEREMPUAN = "PEREMPUAN"
+
 
 app = FastAPI()
 
 @app.get("/simple_api")
 def simple_get():
     return {'message':"simple bgt woi"}
+
+@app.get("/path/gue")
+def path_param():
+    return {'message':"gue"}
+
+@app.get("/path/{gender}")
+def path_param(gender:Gender):
+    if gender is Gender.LAKI_LAKI:
+        print("Laki nih")
+        
+    return {'message':gender}
+
